acd1ebaa84c9002f1d5a5f46433692527deabf17
utensil <utensilcandel@gmail.com>
2019-05-26 17:17:02 +0800

    Auto-install sympy and galgebra if not installed

--- deps/build.jl
+++ deps/build.jl
@@ -1,5 +1,13 @@
-# using PyCall;
+using PyCall;
 
-# if !haskey(ENV, "USE_CUSTEM_GALGEBRA")
-#   run(PyCall.python_cmd(`-m pip install sympy==1.3 -e git+https://github.com/pygae/galgebra.git#egg=galgebra`))
-# end
+try
+  pyimport("sympy")
+catch e
+  run(PyCall.python_cmd(`-m pip install sympy==1.3`))
+end
+
+try
+  pyimport("galgebra")
+catch e
+  run(PyCall.python_cmd(`-m pip install -e git+https://github.com/pygae/galgebra.git#egg=galgebra`))
+end
